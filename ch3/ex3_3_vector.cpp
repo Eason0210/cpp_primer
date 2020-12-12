483ac8a05674419abc64e02b7a759f1d43a92aba
Aqua <aqua0210@163.com>
2020-12-12 14:50:17 +0800

    finished exercise 3.18 vector.

--- ch3/ex3_3_vector.cpp
+++ ch3/ex3_3_vector.cpp
@@ -36,5 +36,44 @@ int main(){
         cout << v << " ";
     cout << endl;
 
+    // 以10分为分数段统计成绩的数量：0~9,10~19,...,90~99,100
+    vector<unsigned> scores(11,0);
+    unsigned grade;
+    while(cin >> grade){
+        if(grade <= 100)
+            ++scores[grade/10];
+        if (getchar() == '\n')  break;
+    }
+
+    cout << "the size of scores is: " << scores.size() << endl;
+    cout << "the elements in the scores are: " << endl;
+
+    for(auto v : scores)
+        cout << v << " ";
+    cout << endl;
+    // 练习3.16 检验3.13 的结果
+
+    // vector<int> v1{10}; // 在c++98 不合法，在c++11 以上是合法的
+    // 可以在编译时加入参数指定C++ 的标准：g++ -std=c++2a -o ex3_3_vector ex3_3_vector.cpp
+    // -std=c++11 11标准
+    // -std=c++17 17标准
+    // =std=c++2a 20标准
+    // vector<int> v1 {10,42}; 同上
+    vector<int> v1 {10,42};
+    cout << "the size of vector: " << v1.size() << endl;
+    cout << "the elements of vector are: " << endl;
+    for(auto v : v1)
+        cout << v << " ";
+    cout << endl;
+
+    // 练习3.18 检验语法
+    vector<int> ivec;
+    // ivec[0] = 42;
+    ivec.push_back(42);
+
+    for(auto v : ivec)
+        cout << v << " ";
+    cout << endl;
+
     return 0;
 }
