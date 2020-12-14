cd7230a676957548b4fae88394b77e7ac9843834
Eason Huang <aqua0210@163.com>
2020-12-14 14:56:17 +0800

    finished exercise 6.4 of 6.1_function.

---
+++ ch6/ex6_1_function.cpp
@@ -0,0 +1,66 @@
+#include <iostream>
+#include <vector>
+#include <string>
+
+using namespace std;
+
+// 练习6.2 修改程序中的错误
+string f() {
+    string s = "hello!";
+    return s;
+}
+
+void f2 (int i){
+    cout << i << endl;
+}
+
+double squre (double x){
+    return x * x;
+}
+
+// 练习6.3 编写 fact 函数
+
+int fact (int val){
+    int rect = 1;
+    while (val > 1){
+        rect *= val--;
+    }
+    return rect;
+}
+
+// 练习6.4 编写交互函数，要求用户输入一个数字，并生成阶乘。
+int ifact (){
+    int ival;
+    int rect = 1;
+    cout << "Please in put an Integer number: " << endl;
+    if(cin >> ival){
+        while (ival > 1)
+            rect *= ival--;
+
+    }else{
+        cout << "You must input an integer." << endl;
+    }
+    return rect;
+}
+
+// 同上，但这里通过直接调用 fact 函数实现
+int i2fact (){
+    int ival;
+    int rect = 1;
+    cout << "Please in put an Integer number: " << endl;
+    if(cin >> ival)
+        rect = fact(ival);
+    else
+        cout << "You must input an integer." << endl;
+    return rect;
+}
+
+int main(){
+    cout << f() << endl;
+    f2(10);
+    cout << squre(5.1) << endl;
+    cout << fact(10) << endl;
+    cout << i2fact() << endl;
+
+    return 0;
+}
