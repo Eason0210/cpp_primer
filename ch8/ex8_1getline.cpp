7b4140071e9c9133a0efd802dc891097cba69cf8
Eason Huang <aqua0210@163.com>
2020-12-21 17:00:38 +0800

    add message when exit program.

--- ch8/ex8_1getline.cpp
+++ ch8/ex8_1getline.cpp
@@ -5,7 +5,10 @@ int main(){
     std::string line;
     std::cout <<"Please input strings and \"EXIT\" to quit: "<< std::endl;
     while (std::getline(std::cin, line)){
-        if (line == "EXIT") break;
+        if (line == "EXIT") {
+            std::cout <<"The Program have been quick!"<< std::endl;
+            break;
+        }
         std::cout <<"Line: " << line << std::endl;
     }
 
